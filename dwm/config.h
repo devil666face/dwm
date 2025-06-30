3916b642d376e67b1f175cf843513c49c45f017b
devil666face <in@d6f.ru>
2025-06-30 19:41:39 +0300

    add gaps and monocle patch

--- dwm/config.h
+++ dwm/config.h
@@ -18,7 +18,7 @@ static const unsigned int gappih = 10;      /* horiz inner gap between windows *
 static const unsigned int gappiv = 10;      /* vert inner gap between windows */
 static const unsigned int gappoh = 10;      /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov = 10;      /* vert outer gap between windows and screen edge */
-static const int smartgaps_fact = 0;        /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
+static const int smartgaps_fact = 1;        /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
 static const int showbar = 1;               /* 0 means no bar */
 static const int topbar = 1;                /* 0 means bottom bar */
 static const int bar_height = 0;            /* 0 means derive from font, >= 1 explicit height */
